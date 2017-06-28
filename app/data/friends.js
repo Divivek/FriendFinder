bc24615354e1bcbf8aa9a10a3ba792eacbf626a8
Vivek Jain <Vivek Jain>
2017-06-28 12:20:55 -0400

    code is not fully working

---
+++ app/data/friends.js
@@ -0,0 +1,155 @@
+// Data structure which will hold the friends dataArray
+var friendsArray = [{
+name   : 'Emily',
+photo  : 'https//www.pexels.com/photo/blonde-haired-woman-in-blue-shirt-y-27411/',
+scores :[
+			'1',
+			'3',
+			'4',
+			'5',
+			'2',
+			'1',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+},
+{
+name   : 'Jacob',
+photo  :'https://www.pexels.com/photo/man-young-happy-smiling-91227/',
+scores :[
+			'2',
+			'4',
+			'3',
+			'3',
+			'2',
+			'1',
+			'3',
+			'3',
+			'2',
+			'3',
+		]
+},
+{
+name   : 'john',
+photo  : 'https://www.pexels.com/photo/man-wearing-blue-denim-buttons-up-long-sleeve-and-blace-frame-eyelgasses-26939/',
+scores :[
+			'3',
+			'2',
+			'5',
+			'1',
+			'2',
+			'1',
+			'4',
+			'2',
+			'5',
+			'3',
+		]
+	
+},
+{
+name   : 'Samantha',
+photo  : 'https://www.pexels.com/photo/woman-in-white-v-neck-shirt-in-selective-focus-photography-157023/',
+scores :[
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'2',
+			'3',
+			'4',
+			'2',
+			'3',
+		]
+},
+{
+
+name   : 'Sinthiya',
+photo  : 'https://www.pexels.com/photo/adult-beautiful-blonde-blur-324658/',
+scores :[
+			'2',
+			'4',
+			'5',
+			'2',
+			'2',
+			'3',
+			'3',
+			'1',
+			'2',
+			'3',
+		]
+},
+{
+
+name   : 'Matt',
+photo  : 'https://www.pexels.com/photo/portrait-of-young-man-251829/',
+scores :[
+			'1',
+			'2',
+			'3',
+			'4',
+			'2',
+			'5',
+			'3',
+			'1',
+			'3',
+			'3',
+		]
+},
+{
+
+name   : 'Jonathan',
+photo  : 'https://www.pexels.com/photo/adult-african-ethnicity-bow-tie-cap-173295/',
+scores :[
+			'4',
+			'3',
+			'3',
+			'2',
+			'5',
+			'4',
+			'1',
+			'3',
+			'5',
+			'3',
+		]
+},
+{
+
+name   : 'Richa',
+photo  : 'https://www.pexels.com/photo/adult-beautiful-blur-brunette-295564/',
+scores :[
+			'2',
+			'5',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4',
+			'2',
+			'2',
+			'1',
+		]
+},
+{
+
+name   : 'Rich',
+photo  : 'https://www.pexels.com/photo/man-wearing-teal-and-black-dress-up-shirt-sitting-on-armchair-65108/',
+scores :[
+			'3',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'3',
+			'2',
+			'4',
+			'3',
+		]
+}
+
+];
+
+module.exports = friendsArray;
