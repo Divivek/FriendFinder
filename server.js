5ef8aca1fe9eb76e09e60f6d43a2bdfe67b8c05b
Vivek Jain <Vivek Jain>
2017-07-03 09:49:05 -0400

    Merge fix api routes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 
 // Configure the Express application
 var app = express();
-//var PORT = process.env.PORT;
-var PORT = 3002;
+var PORT = process.env.PORT;
+//var PORT = 3002;
 // Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
 //app.use(express.static(path.join(__dirname, './app')));
